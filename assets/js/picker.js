c0ead5fc3fc8330f8f5faa72b910c8c2dc9a3e44
mingtsky <247538236@qq.com>
2019-11-17 18:19:45 +0800

    l

--- assets/js/picker.js
+++ assets/js/picker.js
@@ -7,17 +7,11 @@ window.onload = function() {
 	const $picker = document.querySelector('#texture-picker');
 	const $hidePicker = document.querySelector('#hidePicker');
 	const $header = document.querySelector('header');
-<<<<<<< HEAD
-	
-	$picker.onclick = function(e) {
-		const textureName = e.target.id;
-=======
 	$header.setAttribute('class', `texture-qiuri`);
 	
 	$picker.onclick = function(e) {
 		const textureName = e.target.id;
 		// console.log(textureName)
->>>>>>> 修改
 		if (textureName && textureName != 'texture-picker' && textureName != 'hidePicker') {
 			$header.setAttribute('class', `texture-${textureName}`);
 		}
